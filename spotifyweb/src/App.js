0f441e046242fdc581d48cef405b9a4ccb35c693
David Nguyen <David@Davids-Air.hsd1.ma.comcast.net>
2020-02-10 15:51:12 -0500

    Finally got async recommendations to work

--- spotifyweb/src/App.js
+++ spotifyweb/src/App.js
@@ -13,7 +13,7 @@ class Picker extends Component{
         this.state = {
             loggedIn: params.access_token?  true: false,
             gotUserInfo: false,
-            topArtistsTracksGenresChosen: false,
+            personalMusicInfoMode: '',
         }
         if (params.access_token){
             spotifyWebApi.setAccessToken(params.access_token)
@@ -39,18 +39,20 @@ class Picker extends Component{
             })
         })   
     }
-    userPicked() {
+    userPickedFromDropdown(mode) {
         this.setState({
-            topArtistsTracksGenresChosen: true,
+            personalMusicInfoMode: mode
         })
-        // if choose others, then set topArtistsTracksGenresChosen to false
     }
 
     // if choose top artist/genre
     render() {
-        var displayText;
+        var displayText = '';
         if(!this.state.gotUserInfo){
             this.getMe();
+        }
+        if(this.state.personalMusicInfoMode === "ArtistsTracksGenres"){
+            displayText = "Your top artists, tracks, and genres are: ";
         }
         return(
             <div>
@@ -62,76 +64,121 @@ class Picker extends Component{
                         Choose an action
                     </Dropdown.Toggle>
                     <Dropdown.Menu>
-                        <Dropdown.Item onClick={() => this.userPicked()}> Get your top artists, tracks, and genres </Dropdown.Item>
-                        <Dropdown.Item href="#"> b </Dropdown.Item>
+                        <Dropdown.Item onClick={() => this.userPickedFromDropdown("ArtistsTracksGenres")}> Get your top artists, tracks, and genres </Dropdown.Item>
+                        <Dropdown.Item onClick={() => this.userPickedFromDropdown("Recommendations")}> Get your music recommendations </Dropdown.Item>
                         <Dropdown.Item href="#"> c </Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
                 <div>
-                {this.state.topArtistsTracksGenresChosen?displayText = "Your top artists, tracks, and genres are: " : ''}
-                <TopArtistsTracksGenres chosen={this.state.topArtistsTracksGenresChosen} />
+                {displayText}
+                <PersonalMusicInfo mode={this.state.personalMusicInfoMode} />
                 </div>
             </div>
         );
     }
 }
-class TopArtistsTracksGenres extends Picker {
+class PersonalMusicInfo extends Picker {
     constructor(props){
         super(props);
         this.state = {
             topArtistsArray: [],
             topGenreArray: [],
             topTracksArray: [],
+            topArtistsIDArray: [],
+            topGenresIDArray: [],
+            topTracksIDArray: [],
+            recommendedTracks: [],
+            numberOfSeedArtists: Math.round(Math.random() * 5),
+            seedArtistsIDArray: [],
+            seedTracksIDArray: [],
         };
         this.getMyTopArtists();
         this.getMyTopTracks();
     }
 
     getMyTopArtists(){
-        spotifyWebApi.getMyTopArtists()
+        spotifyWebApi.getMyTopArtists({time_range: 'medium_term', limit:10})
         .then((response) => {
-            var topGenreObject = new Object();
-            var topArtistsArray = new Array();
+            var topGenreObjectTemp = new Object();
+            var topArtistsArrayTemp = new Array();
+            var topArtistsIDArrayTemp = new Array();
 
             response.items.forEach(function(element) {
-                topArtistsArray.push(element.name)
+                topArtistsArrayTemp.push(element.name);
+                topArtistsIDArrayTemp.push(element.id);
                 element.genres.forEach(function(genre) {
-                    if (genre in topGenreObject){
-                        topGenreObject[genre] += 1;
+                    if (genre in topGenreObjectTemp){
+                        topGenreObjectTemp[genre] += 1;
                     } else {
-                        topGenreObject[genre] = 1;
+                        topGenreObjectTemp[genre] = 1;
                     }
+
                 })
             });
 
-            var sortedTopGenreArray = new Array();
-            for(var genre in topGenreObject){
-                sortedTopGenreArray.push([genre, topGenreObject[genre]]);
+            var sortedTopGenreArrayTemp = new Array();
+            for(var genre in topGenreObjectTemp){
+                sortedTopGenreArrayTemp.push([genre, topGenreObjectTemp[genre]]);
             }
-            sortedTopGenreArray.sort(function(a,b){
+            sortedTopGenreArrayTemp.sort(function(a,b){
                 return b[1] - a[1];
             });
+
+            var seedArtistsIDArrayTemp = new Array();
+            var randomIndex = Math.round(Math.random() * 9);
+
+            for(let i = 0; i < this.state.numberOfSeedArtists; i++){
+                while (seedArtistsIDArrayTemp.includes(topArtistsIDArrayTemp[randomIndex])){
+                    randomIndex = Math.round(Math.random() * 9);
+                }
+                seedArtistsIDArrayTemp.push(topArtistsIDArrayTemp[randomIndex]);
+            }
             this.setState({
-                topArtistsArray: topArtistsArray.slice(0,5),
-                topGenreArray: sortedTopGenreArray.slice(0,5)
+                topArtistsArray: topArtistsArrayTemp,
+                topGenreArray: sortedTopGenreArrayTemp,
+                topArtistsIDArray: topArtistsIDArrayTemp,
+                seedArtistsIDArray: seedArtistsIDArrayTemp,
             })
         })
-
     }
     getMyTopTracks(){
-        spotifyWebApi.getMyTopTracks()
+        spotifyWebApi.getMyTopTracks({time_range: 'medium_term', limit:10})
         .then((response) => {
-            var topTracksArray = new Array();
+            var topTracksArrayTemp = new Array();
+            var topTracksIDArrayTemp = new Array();
+
             response.items.forEach(function(element){
-                topTracksArray.push(element.name);
+                topTracksArrayTemp.push(element.name);
+                topTracksIDArrayTemp.push(element.id);
             });
+
+            var seedTracksIDArrayTemp = new Array();
+            var randomIndex = Math.round(Math.random() * 9);
+            for(let i = 0; i < 5 - this.state.numberOfSeedArtists; i++){
+                while (seedTracksIDArrayTemp.includes(topTracksIDArrayTemp[randomIndex])){
+                    randomIndex = Math.round(Math.random() * 9);
+                }
+                seedTracksIDArrayTemp.push(topTracksIDArrayTemp[randomIndex]);
+            }
+
             this.setState({
-                topTracksArray: topTracksArray.slice(0,5),
+                topTracksArray: topTracksArrayTemp,
+                topTracksIDArray: topTracksIDArrayTemp,
+                seedTracksIDArray: seedTracksIDArrayTemp,
             })
         })
     }
+
+    getMyRecommendations(){
+        spotifyWebApi.getRecommendations({time_range: 'medium_term', limit:10, seed_artists: this.state.seedArtistsIDArray,
+    seed_tracks: this.state.seedTracksIDArray})
+        .then((response) => {
+            console.log(response)
+        })
+    }
+    
     render() {
-        if (this.props.chosen === true) {
+        if (this.props.mode === "ArtistsTracksGenres") {
             const topArtistsRender = this.state.topArtistsArray.map((element) => <li key = {element}>{element}</li>);
             const topGenresRender = this.state.topGenreArray.map((element) => <li key = {element}>{element[0]} : {element[1]}</li>);
             const topTracksRender = this.state.topTracksArray.map((element) => <li key = {element}>{element}</li>);
@@ -146,6 +193,14 @@ class TopArtistsTracksGenres extends Picker {
                     <div className="topGenresRender">
                         <ul> {topGenresRender} </ul>
                     </div>
+                </div>
+            )
+        } else if (this.props.mode === "Recommendations"){
+            this.getMyRecommendations();
+            const testRender = "test";
+            return(
+                <div>
+                    {testRender}
                 </div>
             )
         } else {
